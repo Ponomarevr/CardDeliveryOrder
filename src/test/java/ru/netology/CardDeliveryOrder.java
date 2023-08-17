be7f37bcbc7b581a4200559f7f4c73573bc0f2c5
Roman Ponomarev <ponomarevr1986@gmail.com>
2023-08-17 16:43:15 +0300

    Test fixed

--- src/test/java/ru/netology/CardDeliveryOrder.java
+++ src/test/java/ru/netology/CardDeliveryOrder.java
@@ -1,37 +1,40 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.*;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CardDeliveryOrder {
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     void shouldCorrectFill() {
         open("http://localhost:9999");
 
-        LocalDate currentDate = LocalDate.now();
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy"); // Формат даты
-        // Добавляем 3 дня к текущей дате
-        LocalDate newDate = currentDate.plusDays(10);
-        // Форматируем новую дату
-        String formattedDate = newDate.format(dateFormatter);
+        String planningDate = generateDate(4, "dd.MM.yyyy");
 
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[placeholder='Город']").setValue("Воронеж");
         $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id=date] input").sendKeys(" ");
-        form.$("[placeholder='Дата встречи']").setValue(formattedDate);
-        form.$("[data-test-id=name] [name=name]").setValue("Иван Иванов-Петров");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[placeholder='Дата встречи']").setValue(planningDate);
+        form.$("[data-test-id=name] [name=name]").setValue("Пиотр Покрышкин-Кожедуб");
         form.$("[data-test-id=phone] [type=tel]").setValue("+79123456789");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$(".button__content").click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(13));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(13))
+                .shouldBe(Condition.visible);
 
     }
 }
